37349c1a65ecabb897a0cffd308b0530f71952ba
Alexey Chebotarev <alexey.chebotarev21@gmail.com>
2017-10-14 23:15:23 +0300

    [1.2.3] Add endpoint for post show Add request tests

--- spec/controllers/api/v1/author/posts_controller_spec.rb
+++ spec/controllers/api/v1/author/posts_controller_spec.rb
@@ -3,6 +3,42 @@ require 'rails_helper'
 describe Api::V1::Author::PostsController, type: :request do
   let(:user)  { create :user, email: 'blog@email.example' }
   let!(:token) { { 'X-Auth-Token': auth_token(user.id) } }
+  let!(:published_at) { '10.10.2016' }
+
+  describe 'GET #show' do
+    let(:post) { create :post, author: user, published_at: '10.10.2016' }
+    let(:params) { { post_id: post.id } }
+
+    context 'when success' do
+      before { get "/api/v1/posts/#{post.id}", headers: json_request_headers.merge!(token) }
+
+      specify do
+        expect(response.body).to eq({
+                                       id: post.id,
+                                       title: post.title,
+                                       body: post.body,
+                                       published_at: Time.parse(published_at),
+                                       author_nickname: user.nickname
+                                    }.to_json)
+
+        expect(response).to have_http_status(200)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+
+    context 'when failure' do
+      before { get '/api/v1/posts/not_id', headers: json_request_headers.merge!(token) }
+
+      specify do
+        expect(response.body).to eq({
+                                       error: { message: 'Post not found' }
+                                    }.to_json)
+
+        expect(response).to have_http_status(406)
+        expect(response.content_type).to eq('application/json')
+      end
+    end
+  end
 
   describe 'POST #create' do
 
@@ -10,7 +46,6 @@ describe Api::V1::Author::PostsController, type: :request do
 
     context 'when success' do
       context 'with published_at' do
-        let(:published_at) { '10.10.2016' }
         let(:params) { { title: 'Title', body: 'Body', published_at: published_at }.to_json }
 
         specify do
@@ -21,6 +56,7 @@ describe Api::V1::Author::PostsController, type: :request do
                                          published_at: Time.parse(published_at),
                                          author_nickname: user.nickname
                                       }.to_json)
+
           expect(response).to have_http_status(201)
           expect(response.content_type).to eq('application/json')
         end
