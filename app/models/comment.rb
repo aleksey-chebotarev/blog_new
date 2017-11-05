34d65f3635e51af088033e161a27995d6843b790
Alexey Chebotarev <alexey.chebotarev21@gmail.com>
2017-11-05 12:53:38 +0300

    Add changes after review:   * Add validation for polymorphic association   * Change method report_build for model user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,17 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :commentable, polymorphic: true
 
+  ALLOWED_TYPES = %w(Post).freeze
+
   before_create :time_now_if_published_at_is_nil
 
   validates :body, presence: true
+
+  validates :commentable_type, presence: true,
+    inclusion: {
+      in: ALLOWED_TYPES,
+      message: "%{value} type is not included in the list"
+    }
 
   private
 
